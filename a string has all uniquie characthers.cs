259cdb8a5b6d23edf1d7c00089d6ba358f6701c0
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-03 21:03:19 +0200

    Add files via upload

---
+++ a string has all uniquie characthers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace a_string_has_all_uniquie_characthers
+{
+    class Program
+    {
+        static bool unique(String str)
+        {
+       
+            // daca numar de 2 ori acelasi caracter, return false
+            for (int i = 0; i < str.Length; i++)
+                for (int j = i + 1; j < str.Length; j++)
+                    if (str[i] == str[j])
+                        return false;
+        
+            return true;
+        }
+
+        public static void Main()
+        {
+            string a = "mirela";
+
+            if (unique(a) == true)
+                Console.WriteLine("stringul are caractere unice " + a);
+            else
+                Console.WriteLine("stringul are caractere duplicate " + a);
+
+            Console.ReadLine();
+        }
+    }
+}
