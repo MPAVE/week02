3111628f4414bbe2c32b976f4427ba6a66e0b9d8
MPAVE <45466369+MPAVE@users.noreply.github.com>
2018-12-03 21:05:48 +0200

    Add files via upload

---
+++ function with prime number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace function_with_prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+        /*Reading number      
+         */
+
+        Console.Write("scrie numarul: ");
+        number = Convert.ToInt32(Console.ReadLine());
+ 
+        int f = 0;
+        int i = 2;
+        while (i <= number / 2)
+        {
+            if (number % i == 0)
+            {
+                f = 1;
+                break;
+            }
+         i++;
+        }
+        if (f == 0)
+           Console.WriteLine(number+" este numar prim");
+        else
+            Console.WriteLine(number + " nu este numar prim");
+ 
+ 
+        Console.ReadLine();
+    }
+
+
+          
+        }
+    }
---
+++ palindrom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti cuvantul:");
+            string a = Console.ReadLine();
+            char[] arr1 = a.ToCharArray();
+            Array.Reverse(arr1);
+
+            string b = new string(arr1);
+
+            if(a.Equals(b))
+            {
+                Console.WriteLine("acesta este palindrom");
+            }
+            else
+            {
+                Console.WriteLine("acesta nu este palindrom");
+            }
+            Console.ReadLine();
+        }
+    }
+}
